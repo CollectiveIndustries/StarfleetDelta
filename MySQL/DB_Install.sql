5462c311613b6085b499799dc19199bf63f8da57
Admiral Morketh <admmorketh@ufgq.collective-industries.net>
2016-08-23 11:11:03 -0500

    Adds preliminary Databse Install files

---
+++ MySQL/DB_Install.sql
@@ -0,0 +1,32 @@
+-- --------------------------------------------------------
+-- Host:                         ci-main2.no-ip.org
+-- Server version:               5.6.27-2 - (Debian)
+-- Server OS:                    debian-linux-gnu
+-- HeidiSQL Version:             9.3.0.5112
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for academy
+DROP DATABASE IF EXISTS `academy`;
+CREATE DATABASE IF NOT EXISTS `academy` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `academy`;
+
+-- Dumping structure for table academy.accounts
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) DEFAULT NULL,
+  `rank` varchar(64) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='hold login information with names and account IDs';
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
