64566233e6bb9536f4e5b80087aa1bed6062d2b0
Root <admiral@Collective-Industries.net>
2016-10-27 22:52:09 -0600

    Adds Database sanity checks for new UFGQ Personell

--- php/clock.php
+++ php/clock.php
@@ -1,18 +1,37 @@
 <?php
 	include("config.php");
 	$uuid = $_POST['uuid'];
-	echo "$uuid Clocked in/out\n\n";
+	$name = $_POST['name'];
 
-	$sql = "INSERT INTO `ufgq`.`Time Clock` (`uuid`) VALUES ('$uuid')";
-	if (mysqli_query($db, $sql))
+// SQL statments
+	$NEW_AVY_SQL = "INSERT INTO `ufgq`.`accounts` (`UUID`, `username`) VALUES ('$uuid', '$name')";
+	$SelectAV = "SELECT ID FROM `ufgq`.`accounts` WHERE `UUID` = '$uuid'";
+	$OnFileInsert = "INSERT INTO ufgq.`Time Clock` (user_id) SELECT id FROM accounts a WHERE a.`UUID` = '$uuid'";
+
+	$result = mysqli_query($db,$SelectAV);
+	$count = mysqli_num_rows($result);
+	if ($count == 0)
 	{
-		echo "Time Clock was updated.";
+		if(!mysqli_query($db,$NEW_AVY_SQL))
+		{
+			echo "ERROR: " . $sql . "\n" . mysqli_error($db);
+			die("\n\nFailed to update Users Time Card.\nContact Captain Morketh Sorex UFGQ IT Department with the provided Error Message\nFor bug Reports/Updates\nhttps://github.com/CollectiveIndustries/UFGQ/issues");
+		}
 		mysqli_commit();
+		echo "New Account created. User now logged in.";
 	}
 	else
 	{
-		echo "Error: " . $sql . "\n" . mysqli_error($db);
-		die("\n\nFailed to Clock User in/out.\nContact Captain Morketh Sorex UFGQ IT Department with the provided Error Message\nFor bug Reports/Updates\nhttps://github.com/CollectiveIndustries/UFGQ/issues");
+		echo "Account on file. Time Clock updated.";
 	}
+// Once the Account is on file we can just log the user right in
+	mysqli_query($db,$OnFileInsert);
+
+//	else
+//	{
+//		echo "Error: " . $sql . "\n" . mysqli_error($db);
+//		die("\n\nFailed to Clock User in/out.\nContact Captain Morketh Sorex UFGQ IT Department with the provided Error Message\nFor bug Reports/Updates\nhttps://github.com/CollectiveIndustries/UFGQ/issues");
+//	}
+
 	mysqli_close($db);
 ?>
