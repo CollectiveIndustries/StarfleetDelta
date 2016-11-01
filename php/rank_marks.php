f8d48dd0424032d4b1c5a9faa87e0c3160ac4a12
Root <admiral@Collective-Industries.net>
2016-11-01 00:11:08 -0600

    Adds Rank marks page and Stats interface.

---
+++ php/rank_marks.php
@@ -0,0 +1,37 @@
+<?php
+	header('Content-type: text/html; charset=utf-8');
+        include("config.php");
+        mysqli_set_charset($db,"utf8");
+
+?>
+<html>
+	<meta charset="utf-8"/>
+	<center>
+	<body>
+	<table>
+        	<tr>
+               		<th>Rank Name</th>
+                        <th>Rank Logo</th>
+                </tr>
+<?php
+        header('Content-type: text/html; charset=utf-8');
+
+	$query = "SELECT rname, RankLogo FROM `ufgq`.`Rank`"; //You don't need a ; like you do in SQL
+	$result = mysqli_query($db,$query);
+
+//	echo "<table>"; // start a table tag in the HTML
+
+	while($row = mysqli_fetch_array($result))
+	{   //Creates a loop to loop through results
+		echo "<tr><td>" . $row['rname'] . "</td><td>" . $row['RankLogo'] . "</td></tr>";  //$row['index'] the index here is a field name
+	}
+
+//	echo "</table>"; //Close the table in HTML
+
+	mysqli_close(); //Make sure to close out the database connection
+?>
+</tr>
+</table>
+</body>
+</center>
+</html>
---
+++ php/stats.php
@@ -0,0 +1,25 @@
+<?php
+	header('Content-type: text/html; charset=utf-8');
+	include("config.php");
+	mysqli_set_charset($db,"utf8");
+
+	$type = $_POST['stat'];
+
+	if($type == "rank")
+	{
+		echo "Rank | Numer of People\n";
+		$query = "SELECT r.rname AS `name`, COUNT(*) AS `people` FROM `Rank` r INNER JOIN `accounts` a ON a.RankID=r.RankID GROUP BY r.RankID ORDER BY r.RankID";
+	}
+	elseif( $type == "division")
+	{
+		echo "Division | Numer of People\n";
+		$query = "SELECT d.dname AS `name`, COUNT(*) AS `people` FROM `divisions` d INNER JOIN `accounts` a ON a.DivID=d.did GROUP BY d.did ORDER BY name";
+	}
+
+        $result = mysqli_query($db,$query);
+
+        while($row = mysqli_fetch_array($result))
+        {   //Creates a loop to loop through results
+                echo $row['name'] . " | " . $row['people'] . "\n";  //$row['index'] the index here is a field name
+        }
+?>
