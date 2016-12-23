a7a2baadd258c9e18590b672d460a5a41e11bbd2
Morketh <admmorketh@gmail.com>
2016-12-22 18:08:35 -0700

    Finishes database migration sysyem

--- bash/merge_db.sh
+++ bash/merge_db.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-
+mysql -e "DROP DATABASE IF EXISTS \`StarfleetDelta\`;CREATE DATABASE \`StarfleetDelta\`;"
+for table in `mysql -B -N -e "SHOW TABLES;" StarfleetDelta-1` do
+  mysql -e "RENAME TABLE \`StarfleetDelta-1\`.\`$table\` to \`StarfleetDelta\`.\`$table\`"
+done
+mysql -e "DROP DATABASE \`StarfleetDelta-1\`;"
